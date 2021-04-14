62e1dfe6413e6935e3a87408143ab439a5281bbf
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-04-13 23:21:24 -0500

    try js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 function init(){
 //add your javascrip between these two lines of code
  
+	var eBut = document.getElementById("entrybutton");
 
-
-
-
-
-
-
+	function runAlert() {
+		var eInp = document.getElementById("entryinput");
+		var tOut = document.getElementById("textoutput");
+		
+		alert("Lev Pesin: " + eInp.value);
+		tOut.innerHTML = eInp.value;
+	}
+	
+	eBut.addEventListener('click', runAlert);
+}
 window.addEventListener('load', init);
